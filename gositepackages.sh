0e53ce5e8fbdfa57bf7f5e4718a8e69db7d9ef07
jachinpy <1689313450@qq.com>
2015-02-23 23:45:32 +0800

    add virtualenv-name if workon ENV for site-packages

--- gositepackages.sh
+++ gositepackages.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 gosite () {
-  cd $HOME/.virtualenvs/$1/lib/python2.7/site-packages
+    if [[ $? -eq 0 ]]
+        then
+        cd $VIRTUAL_ENV/lib/python2.7/site-packages
+    return 1
+    fi
+    cd $HOME/.virtualenvs/$1/lib/python2.7/site-packages
 }
