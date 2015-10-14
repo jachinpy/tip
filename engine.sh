ac376e45fd921e3251622778955850cf7005bec8
jachinpy <1689313450@qq.com>
2015-10-14 13:27:56 +0800

    change method name

--- engine.sh
+++ engine.sh
@@ -5,7 +5,7 @@ basedirscript=$(cd "$(dirname "$0")"; pwd)
 
 array_pc=(django git python byobu tmux tmuxinator markdown scbl zsh locales)
 
-PcPluginsConfig ()
+Loading_plugins ()
 {
     for i in ${array_pc[@]}
     do
@@ -14,5 +14,5 @@ PcPluginsConfig ()
     done
 }
 
-PcPluginsConfig
+Loading_plugins
 source "$basedirscript/migrate.sh"
