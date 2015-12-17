2edc8d31337ce70a24448bf556bf6304780dc046
jachinpy <1689313450@qq.com>
2015-12-17 14:36:01 +0800

    add picture coding/ vimdiff config

--- plugins/zsh/utils.zsh
+++ plugins/zsh/utils.zsh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cmdcount () {
     history | awk '{CMD[$2]++;count++;} END { for (a in CMD )print CMD[a] " " CMD[a]/count*100 "% " a }' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n10
 }
@@ -16,3 +17,5 @@ getallwebsite () {
 pythonserver () {
     python -m SimpleHTTPServer 9090
 }
+
+alias jpgmv="python ~/utils/tip/plugins/codingutils/coding.py"
