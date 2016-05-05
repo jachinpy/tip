a72a2290fbd8b88e3e73e91457d24dc080466ac7
jachinpy <1689313450@qq.com>
2016-05-05 11:25:59 +0800

    add 360doc download curl

--- engine.sh
+++ engine.sh
@@ -3,7 +3,7 @@
 basedirscript=$(cd "$(dirname "$0")"; pwd)
 
 
-array_pc=(django git python byobu tmux tmuxinator markdown scbl zsh locales emacs vim)
+array_pc=(django git python byobu tmux tmuxinator markdown scbl zsh locales emacs vim 360library)
 
 Loading_plugins ()
 {
---
+++ plugins/360library/save.zsh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+alias 360doc="curl $1 > ~/tmp/360doc_download.html"
