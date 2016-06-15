aedfa517b4fab6f389adebdf37e3defb2678e13f
jachinpy <1689313450@qq.com>
2016-06-15 17:53:46 +0800

    update tmux command

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-alias tnw="tmux new-windows"
+alias tnw="tmux new-window"
 alias tlw="tmux list-windows"
-alias tsw="tmux select-windows -t"
+alias tsw="tmux select-window -t"
 alias trw="tmux rename-window"
 alias tlk="tmux list-keys"
 alias tlc="tmux list-commands"
