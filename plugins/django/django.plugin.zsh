fc79623fe876eec6292e77f7e8d1d1fda9e55e34
jachinpy <1689313450@qq.com>
2016-06-28 18:53:01 +0800

    add test env checkout for django

--- plugins/django/django.plugin.zsh
+++ plugins/django/django.plugin.zsh
@@ -6,3 +6,36 @@ alias jr='python manage.py runserver'
 alias jt="python manage.py test "
 alias jd="python manage.py dbshell"
 alias jsy="python manage.py syncdb"
+
+# 测试用例环境脚本, 默认文件名是 .env
+DEFAULT_DJANGO_TEST_ENV='.env'
+
+# 第一次创建 .env后，手动备份文件
+function env-backup () {
+	cp .env .env.orig　
+}
+
+# 初始化环境
+function env-init () {
+	cp .env .env.prod
+	cp .env .env.dev
+	cp .env .env.tes
+}
+
+# checkout abbr. 切换环境
+function env-co () {
+	cp $1 .env
+	cat .env
+}
+
+
+# list test env.
+function env-list () {
+	ls .env*
+}
+
+alias ecd='echo "你现在处于　dev 分支。" && env-co .env.dev'
+alias ecp='echo "你现在处于　prod 分支。" && env-co .env.prod'
+alias ect='echo "你现在处于　test 分支。" && env-co .env.tes'
+alias ecl='env-list'
+
