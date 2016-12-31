18e01c45290a9b7d03aa57ae601f49ca7ef58b2b
jachinpy <1689313450@qq.com>
2016-12-31 19:19:57 +0800

    add git shortlog summary commits

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+_git_shortlog_sum_commit () {
+    # You can choose any one.
+    git shortlog -s | awk '{sum+=$1} END {print "Current repo Summary Commits:", sum}'
+    # glog --no-merges| wc -l | xargs echo "Current repo Summary Commits:"
+}
+
+
 alias gh="git hist"
 alias ghh="git hist --max-count=1"
 alias gfp="git fetch -p"
@@ -7,3 +15,4 @@ alias gffs="git flow feature start "
 alias gcfp="git cat-file -p "
 alias gcft="git cat-file -t "
 alias gct="git checkout --track "
+alias gsum="_git_shortlog_sum_commit"
