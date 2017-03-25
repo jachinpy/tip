1a5b39186d054c73dc52b4087f9144de63598a31
jachinpy <1689313450@qq.com>
2017-03-26 01:17:18 +0800

    update virtualenvwrapper

--- plugins/virtualenvwrapper/virtualenvwrapper.zsh
+++ plugins/virtualenvwrapper/virtualenvwrapper.zsh
@@ -1,6 +1,11 @@
 if [ ! -d "$HOME/.virtualenvs" ];then
     mkdir $HOME/.virtualenvs
 fi
+if [ -f "/usr/bin/virtualenvwrapper.sh" ];then
+    if [ ! -f "/usr/local/bin/virtualenvwrapper.sh" ];then
+        sudo ln -s /usr/bin/virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh
+    fi
+fi
 
 export WORKON_HOME=$HOME/.virtualenvs
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
